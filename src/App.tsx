e9566e5fd13549b08f268d486f168cf6af9bbec5
John Daffue <daffuejhn@gmail.com>
2026-02-12 12:06:16 +0000

    Init project
    Added Tailwind, React, XState, Vite.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <div className={'justify-center'}><p className={"font-bold text-2xl"}>Tailwind is working!!</p></div>
+    </>
+  )
+}
+
+export default App
